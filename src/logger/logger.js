d7716bfac5fb2d5a6fc0b3430a2b7545236ead78
ChanduKu <kuntrapakuc@gmail.com>
2022-04-06 19:16:06 +0530

    pushesed the first file

---
+++ src/logger/logger.js
@@ -0,0 +1,4 @@
+let lg=function(){
+console.log("welcome to my application .I am K.CHandu and a part of FunctionUp uranium cohort")
+}
+module.exports.logs=lg
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,9 +1,18 @@
 const express = require('express');
 
+const loggs=require("../logger/logger")
+
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
-    res.send('My first ever api!')
+    console.log(loggs.logs)
+    res.send('My first ever api! bro')
+});
+router.get('/test-me2', function (req, res) {
+    res.send('My first ever api! second one  bro')
+});
+router.get('/chandu', function (req, res) {
+    res.send('HI chandu i add your name to the url don cool')
 });
 
 module.exports = router;
